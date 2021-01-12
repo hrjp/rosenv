6d4f38226f3cd3e4cd4c87741baaf350147c9d06
Hara-kcct <r115159@g.kobe-kosen.ac.jp>
2021-01-12 13:09:55 +0900

    first commit

---
+++ package_install.bash
@@ -0,0 +1,19 @@
+#!/bin/bash
+export ROS_VER=kinetic 
+sudo apt-get install -y ros-$ROS_VER-velodyne
+sudo apt-get install -y ros-$ROS_VER-joy
+sudo apt-get install -y ros-$ROS_VER-joystick-drivers
+sudo apt-get install -y ros-$ROS_VER-jsk-visualization
+sudo apt-get install -y ros-$ROS_VER-image-proc
+sudo apt-get install -y ros-$ROS_VER-realsense2-camera
+sudo apt-get install -y ros-$ROS_VER-move-base
+sudo apt-get install -y ros-$ROS_VER-gmapping
+sudo apt-get install -y ros-$ROS_VER-amcl
+sudo apt-get install -y ros-$ROS_VER-mcl-3dl
+sudo apt-get install -y ros-$ROS_VER-urg-node
+sudo apt-get install -y ros-$ROS_VER-turtlesim
+sudo apt-get install -y ros-$ROS_VER-rosserial-arduino
+sudo apt-get install -y ros-$ROS_VER-rosserial
+sudo apt-get install -y python-numpy python-scipy python-matplotlib ipython ipython-notebook python-pandas python-sympy python-nose
+sudo apt-get install -y ros-$ROS_VER-map-server
+
