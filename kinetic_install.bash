943a377ce6c02088150ec49e60cab6ad34087464
hrjp <r115159@g.kobe-kosen.ac.jp>
2021-02-26 14:42:11 +0900

    chmod

--- kinetic_install.bash
+++ kinetic_install.bash
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 export ROS_VER=kinetic 
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://ha.pool.sks-keyservers.net:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
