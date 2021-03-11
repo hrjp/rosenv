9ea90f52938191cab9e6896d680e19bd30190f2d
hrjp <r115159@g.kobe-kosen.ac.jp>
2021-03-11 11:26:03 +0900

    uvc camera add

--- package_install.bash
+++ package_install.bash
@@ -20,5 +20,5 @@ sudo apt-get install -y ros-$ROS_VER-diff-drive-controller
 sudo apt-get install -y ros-$ROS_VER-ros-control
 sudo apt-get install -y ros-$ROS_VER-ros-controllers
 sudo apt-get install -y ros-$ROS_VER-gazebo-ros-control
-
+sudo apt-get install -y ros-$ROS_VER-uvc-camera 
 
