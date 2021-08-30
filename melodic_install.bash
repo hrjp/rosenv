0706246c38e08dfcec43b569303c419e20b38395
hrjp <r115159@g.kobe-kosen.ac.jp>
2021-08-30 22:41:17 +0900

    install bash test

--- melodic_install.bash
+++ melodic_install.bash
@@ -1,16 +1,22 @@
 #!/bin/bash
 
 export ROS_VER=melodic
+
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver 'hkp://ha.pool.sks-keyservers.net:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
-sudo apt-get update
-sudo apt-get install -y ros-melodic-desktop-full
-sudo rosdep init
-rosdep update
+sudo apt install curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+sudo apt update
+sudo apt install ros-melodic-desktop-full
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
+sudo apt install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+sudo apt install python-rosdep
+sudo rosdep init
+rosdep update
 source /opt/ros/melodic/setup.bash
 sudo apt-get install -y python-rosinstall
+sudo apt install python-catkin-tools
+
 
 cd
 mkdir -p ~/catkin_ws/src
