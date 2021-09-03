b0a0028c0f41c9aaf20e8467267411f83d111650
hrjp <r115159@g.kobe-kosen.ac.jp>
2021-09-03 23:57:02 +0900

    terminator add

--- software_setup.bash
+++ software_setup.bash
@@ -5,5 +5,5 @@ sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder
 sudo apt-get update
 sudo apt-get install -y simplescreenrecorder
 sudo apt install -y curl
-
+sudo apt-get install -y terminator
 
