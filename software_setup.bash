0274d21ac737b6fe38fd13706d4c179b195ef220
hrjp <r115159@g.kobe-kosen.ac.jp>
2021-09-03 23:47:46 +0900

    file add

---
+++ software_setup.bash
@@ -0,0 +1,9 @@
+sudo apt update
+sudo apt install -y git
+sudo apt install -y ibus-mozc
+sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder
+sudo apt-get update
+sudo apt-get install -y simplescreenrecorder
+sudo apt install -y curl
+
+
