4d9fefa8c3805636735cd9c46d66874af9e34c04
hrjp <r115159@g.kobe-kosen.ac.jp>
2021-10-09 21:17:17 +0900

    bug fix

--- gtsam_install.bash
+++ gtsam_install.bash
@@ -3,7 +3,7 @@
 mkdir gtsam
 wget -O ~/gtsam/gtsam.zip https://github.com/borglab/gtsam/archive/4.0.0-alpha2.zip
 cd ~/gtsam/ && unzip gtsam.zip -d ~/gtsam/
-cd ~/Downloads/gtsam-4.0.0-alpha2/
+cd ~/gtsam/gtsam-4.0.0-alpha2/
 mkdir build && cd build
 cmake ..
 sudo make install
