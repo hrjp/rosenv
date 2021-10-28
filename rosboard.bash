f5cde6754ac1feadfe98ace34432a7692247130a
hrjp <r115159@g.kobe-kosen.ac.jp>
2021-10-28 22:32:11 +0900

    rosborad update

--- rosboard.bash
+++ rosboard.bash
@@ -10,7 +10,7 @@ sudo pip3 install rospkg
 sudo pip3 install psutil
 
 if [ ! -d "/home/catkin_ws/src" ]; then
-
+    echo
 else
     cd /home/catkin_ws/src
     git clone https://github.com/dheera/rosboard
@@ -19,8 +19,9 @@ else
     source devel/setup.bash
 
 fi
+
 if [ ! -d "/home/ros1_ws/src" ]; then
-
+    echo
 else
     cd /home/catkin_ws/src
     git clone https://github.com/dheera/rosboard
@@ -28,8 +29,9 @@ else
     catkin build
     source devel/setup.bash
 fi
-if [ ! -d "/home/$USER/catkin_ws/src" ]; then
 
+if [ ! -d "/home/$USER/catkin_ws/src" ]; then
+    echo
 else
     cd /home/catkin_ws/src
     git clone https://github.com/dheera/rosboard
