95b2bef3ce9616d387c63391965cf428160b3278
hrjp <r115159@g.kobe-kosen.ac.jp>
2021-10-28 22:29:24 +0900

    rosboard update

--- rosboard.bash
+++ rosboard.bash
@@ -7,19 +7,34 @@ sudo apt install -y python3-pip
 sudo pip3 install tornado
 sudo pip3 install simplejpeg
 sudo pip3 install rospkg
+sudo pip3 install psutil
 
-if [-d "/home/catkin_ws/src" ]; then
+if [ ! -d "/home/catkin_ws/src" ]; then
+
+else
     cd /home/catkin_ws/src
     git clone https://github.com/dheera/rosboard
+    cd ..
+    catkin build
+    source devel/setup.bash
+
 fi
-if [-d "/home/ros1_ws/src" ]; then
+if [ ! -d "/home/ros1_ws/src" ]; then
+
+else
     cd /home/catkin_ws/src
     git clone https://github.com/dheera/rosboard
+    cd ..
+    catkin build
+    source devel/setup.bash
 fi
-if [-d "/home/$USER/catkin_ws/src" ]; then
+if [ ! -d "/home/$USER/catkin_ws/src" ]; then
+
+else
     cd /home/catkin_ws/src
     git clone https://github.com/dheera/rosboard
+    cd ..
+    catkin build
+    source devel/setup.bash
 fi
-cd ..
-catkin build
-source devel/setup.bash
+
