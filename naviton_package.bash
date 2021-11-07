575374c8ffc3c8c07d334f536da3a04dd134f5e5
hrjp <r115159@g.kobe-kosen.ac.jp>
2021-11-07 20:50:13 +0900

    naviton_package update

--- naviton_package.bash
+++ naviton_package.bash
@@ -1,4 +1,30 @@
 #!/bin/bash
+
+
+if [ ! -d "/home/catkin_ws/src" ]; then
+    echo
+else
+    cd /home/catkin_ws/src
+fi
+
+if [ ! -d "/home/ros1_ws/src" ]; then
+    echo
+else
+    cd /home/ros1_ws/src
+fi
+
+if [ ! -d "/home/$USER/catkin_ws/src" ]; then
+    echo
+else
+    cd /home/$USER/catkin_ws/src
+fi
+
+if [ ! -d "/home/$USER/ros1_ws/src" ]; then
+    echo
+else
+    cd /home/$USER/ros1_ws/src
+fi
+
 cd catkin_ws/src
 git clone https://github.com/hrjp/kcctcore
 git clone https://github.com/hrjp/kcctnavigation
@@ -6,7 +32,7 @@ git clone https://github.com/hrjp/waypoint_tools
 git clone https://github.com/hrjp/kcctsim
 git clone https://github.com/hrjp/kcctplugin
 git clone https://github.com/hrjp/LeGO-LOAM
-git clone -b noetic https://github.com/iralabdisco/ira_laser_tools
+git clone https://github.com/hrjp/ira_laser_tools
 cd ..
 catkin build
 source devel/setup.bash
