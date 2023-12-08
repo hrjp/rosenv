6b46ed71ccb5a95f2b6b4ff3266b27af0e1ea29e
Shunya Hara <r115159@g.kobe-kosen.ac.jp>
2023-12-08 15:41:30 +0900

    Update software_setup.bash

--- software_setup.bash
+++ software_setup.bash
@@ -2,9 +2,6 @@
 sudo apt update
 sudo apt install -y git
 sudo apt install -y ibus-mozc
-sudo add-apt-repository ppa:maarten-baert/simplescreenrecorder
-sudo apt-get update
-sudo apt-get install -y simplescreenrecorder
 sudo apt install -y curl
 sudo apt-get install -y terminator
 sudo apt install -y net-tools iputils-ping
